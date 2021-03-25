b78691ac54a98d716b7ceeb73755347a3292b5bb
KashiPrg <kashiwagi4652@gmail.com>
2021-03-25 18:57:31 +0900

    各種正規表現を調整

--- main.py
+++ main.py
@@ -165,40 +165,45 @@ class MyFileDropTarget(wx.FileDropTarget):
     # このリストに含まれる正規表現に当てはまる文字列は無視する
     __ignore_lines = [
         r"^\s*ACM Trans",
-        r"^\s*[0-9]:[0-9]\s*$",     # ページ数
-        r"^\s*[0-9]+\s*of\s*[0-9]+\s*$",     # ページ数
-        r"•$",
+        r"^\s*[0-9]*:[0-9]*\s*$",   # ページ数
+        r"^\s*[0-9]*\s*of\s*[0-9]*\s*$",     # ページ数
+        r"^\s*.\s*$",   # なにか一文字しか無い 後でユーザが何文字まで無視するか設定できるようにしたい
+        r"^.*(.{1,3}\s+){10,}.*$",  # ぶつ切れの語が極端に多い
         r"Peng Wang, Lingjie Liu, Nenglun Chen, Hung-Kuo Chu, Christian Theobalt, and Wenping Wang$",
         r"^\s*Multimodal Technologies and Interact\s*",
         r"^www.mdpi.com/journal/mti$",
-        r"^\s*Li et al.\s*$",
+        r"^\s*Li et al\.\s*$",
         r"^\s*Exertion-Aware Path Generation\s*$"
     ]
     # このリストに含まれる正規表現に当てはまる文字列がある行で改行する
     __return_lines = [
         r"(\.|:|;|\([0-9]+\))\s*$",   # 文末(計算式や箇条書きなども含む)
         r"^\s*([0-9]+\s*\.\s*)+.{3,45}\s*$",    # 見出し
-        r"[0-9]+\s*\.?\s*introduction",
-        r"[0-9]+\s*\.?\s*related works?",
-        r"[0-9]+\s*\.?\s*overview",
-        r"[0-9]+\s*\.?\s*algorithm",
-        r"[0-9]+\s*\.?\s*experimental results",
-        r"[0-9]+\s*\.?\s*conclusions",
-        r"^acknowledgements$",
-        r"^references?$"
+        r"^\s*([0-9]+\s*\.\s*)*[0-9]+\s*.{3,45}\s*$",    # 見出し ↑よりも条件が緩いので注意
+        r"^([0-9]+\s*\.?)?\s*introduction$",    # 数字がない場合にも対応
+        r"^([0-9]+\s*\.?)?\s*related works?$",
+        r"^([0-9]+\s*\.?)?\s*overview$",
+        r"^([0-9]+\s*\.?)?\s*algorithm$",
+        r"^([0-9]+\s*\.?)?\s*experimental results?$",
+        r"^([0-9]+\s*\.?)?\s*conclusions?$",
+        r"^([0-9]+\s*\.?)?\s*acknowledgements?$",
+        r"^([0-9]+\s*\.?)?\s*references?$"
     ]
     # markdown方式で出力する時、この正規表現に当てはまる行を見出しとして扱う
     __header_lines = [
-        r"^\s*([0-9]+\s*\.\s*)+.{3,45}\s*$"     # "1.2.3. aaaa" などにヒット
+        r"^\s*([0-9]+\s*\.\s*)+.{3,45}\s*$",     # "1.2.3. aaaa" などにヒット
+        r"^\s*([0-9]+\s*\.\s*)*[0-9]+\s*.{3,45}\s*$"     # "1.2.3 aaaa" などにヒット
     ]
     # 見出しの大きさを決定するためのパターン
     # header_linesの要素と対応する形で記述する
     __header_depth_count = [
-        r"[0-9]+\s*\.\s*"  # 1.2.3. の"数字."の数が多いほど見出しが小さくなる(#の数が多くなる)
+        r"[0-9]+\s*\.\s*",  # 1.2.3. の"数字."の数が多いほど見出しが小さくなる(#の数が多くなる)
+        r"[0-9]+"
     ]
     # 見出しの最大の大きさ 1が最大で6が最小
     # header_linesの要素と対応する形で記述する
     __header_max_size = [
+        2,
         2
     ]
     # このリストに含まれる正規表現に当てはまる文字列があるとき、
@@ -223,10 +228,12 @@ class MyFileDropTarget(wx.FileDropTarget):
     # 一般的な表現の場合は諸刃の剣となるので注意
     # また、リストの最初に近いほど優先して処理される
     __replace_source = [
-        r"\s*[0-9]+\s*of\s*[0-9]+\s*"   # "1 of 9" などのページカウント
+        r"\s*[0-9]+\s*of\s*[0-9]+\s*",   # "1 of 9" などのページカウント
+        r"\s*Li et al\.\s*"
     ]
     # ノイズをどのような文字列に置換するか
     __replace_target = [
+        " ",
         " "
     ]
     __add_japanese_return = True    # 日本語の文章において一文ごとに改行
